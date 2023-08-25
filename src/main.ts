72c93e021476500c2c825c8a83727680958bb3b3
Theo Berkhout <theoberkhout59@gmail.com>
2023-08-25 16:52:51 +0200

    :heavy_plus_sign: add firebase

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,12 @@
+import { initializeApp } from "firebase/app";
+import { environment } from './environment';
+
+// Your web app's Firebase configuration
+const firebaseConfig = environment.firebaseConfig;
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
 function helloWord() {
     return `
         <h1>Hello Jetdev</h1>
