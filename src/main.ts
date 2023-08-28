de58e67fdcd559200f4945164cd88c10032255c0
Theo Berkhout <theoberkhout59@gmail.com>
2023-08-28 11:52:58 +0200

    :sparkles: add message form

--- src/main.ts
+++ src/main.ts
@@ -5,15 +5,31 @@ import { environment } from './environment';
 const firebaseConfig = environment.firebaseConfig;
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+initializeApp(firebaseConfig);
 
-function helloWord() {
+const listenMessageForm = () => {
+    document.getElementById('send-message-form')?.addEventListener('submit', () => {
+        const input: HTMLInputElement | null = document.getElementById('send-message-input');
+        const inputValue = input?.value;
+        alert(inputValue);
+    });
+}
+
+function render() {
     return `
         <h1>Hello Jetdev</h1>
         <p>Juste ici, on va brancher une Realtime Database de Firebase pour faire un chat basique entre deux onglets en 1h</p>
+        <div class="chat-container">
+            <div class="messages"></div>
+            <form id="send-message-form" class="send-message">
+                <input id="send-message-input" type="text" placeholder="Envoyer un message">
+                <button type="submit">Envoyer</button>
+            </form>
+        </div>
     `;
 }
 
 window.onload = () => {
-    document.body.innerHTML = helloWord();
+    document.body.innerHTML = render();
+    listenMessageForm();
 };
