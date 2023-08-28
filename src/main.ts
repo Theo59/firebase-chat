fd37bfb8d1fbab7482df1c77d4cc87e92ce80d21
Theo Berkhout <theoberkhout59@gmail.com>
2023-08-28 16:06:18 +0200

    :sparkles: display messages from Firebase

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,11 @@
-import { initializeApp } from "firebase/app";
+import { initializeApp } from 'firebase/app';
 import { environment } from '../environment';
-import { getDatabase, ref, set } from "firebase/database";
+import { getDatabase, onValue, ref, set } from 'firebase/database';
+
+interface Message {
+    date: number;
+    message: string;
+}
 
 // Your web app's Firebase configuration
 const firebaseConfig = environment.firebaseConfig;
@@ -10,7 +15,7 @@ initializeApp(firebaseConfig);
 const db = getDatabase();
 
 const listenMessageForm = () => {
-    const formEl: HTMLFormElement = document.getElementById('send-message-form')
+    const formEl: HTMLFormElement = document.getElementById('send-message-form');
     formEl?.addEventListener('submit', (e) => {
         e.preventDefault();
         const input: HTMLInputElement | null = document.getElementById('send-message-input');
@@ -23,17 +28,40 @@ const listenMessageForm = () => {
         })
             .then(() => formEl.reset())
             .catch(e => {
-            console.log(e);
-        })
+                console.log(e);
+            });
     });
-}
+};
+
+
+const getMessages = () => {
+    const messagesRef = ref(db, 'messages');
+    onValue(messagesRef, (snapshot) => {
+        const value = snapshot.val();
+        const messages: Message[] = Object.values(value);
+        renderMessages(messages);
+    });
+};
+const renderMessages = (messages: Message[]) => {
+    const messagesElement = document.getElementById('messages');
+    messagesElement.innerHTML = '';
+    messages.forEach((message: Message) => {
+        const newMessage = document.createElement('div');
+        newMessage.innerHTML = `
+            <div class="chat-message-bubble">
+                <span>${message.message}</span> 
+            </div>
+        `;
+        messagesElement.appendChild(newMessage);
+    });
+};
 
 function render() {
     return `
         <h1>Hello Jetdev</h1>
         <p>Juste ici, on va brancher une Realtime Database de Firebase pour faire un chat basique entre deux onglets en 1h</p>
         <div class="chat-container">
-            <div class="messages"></div>
+            <div id="messages"></div>
             <form id="send-message-form" class="send-message">
                 <input id="send-message-input" type="text" placeholder="Envoyer un message">
                 <button type="submit">Envoyer</button>
@@ -44,5 +72,6 @@ function render() {
 
 window.onload = () => {
     document.body.innerHTML = render();
+    getMessages();
     listenMessageForm();
 };
