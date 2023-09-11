9f88d25fead5466737a5e88a7bb2daa57a0779da
Theo59 <theoberkhout59@gmail.com>
2023-09-11 09:09:15 +0200

    :recycle: move files to src folder

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { environment } from '../environment';
 import { getDatabase, onValue, ref, set } from 'firebase/database';
 
 interface Message {
+    user: string;
     date: number;
     message: string;
 }
@@ -14,16 +15,18 @@ const firebaseConfig = environment.firebaseConfig;
 initializeApp(firebaseConfig);
 const db = getDatabase();
 
+let nickname: string = '';
 const listenMessageForm = () => {
-    const formEl: HTMLFormElement = document.getElementById('send-message-form');
-    formEl?.addEventListener('submit', (e) => {
+    const formEl: HTMLFormElement = document.getElementById('send-message-form') as HTMLFormElement;
+    formEl.addEventListener('submit', (e) => {
         e.preventDefault();
-        const input: HTMLInputElement | null = document.getElementById('send-message-input');
+        const input: HTMLInputElement = document.getElementById('send-message-input') as HTMLInputElement;
         const inputValue = input?.value;
         const date = new Date().getTime();
 
         set(ref(db, `messages/${date}`), {
             date: date,
+            user: nickname,
             message: inputValue,
         })
             .then(() => formEl.reset())
@@ -32,7 +35,6 @@ const listenMessageForm = () => {
             });
     });
 };
-
 
 const getMessages = () => {
     const messagesRef = ref(db, 'messages');
@@ -44,16 +46,31 @@ const getMessages = () => {
 };
 const renderMessages = (messages: Message[]) => {
     const messagesElement = document.getElementById('messages');
-    messagesElement.innerHTML = '';
-    messages.forEach((message: Message) => {
-        const newMessage = document.createElement('div');
-        newMessage.innerHTML = `
+    if (messagesElement) {
+        messagesElement.innerHTML = '';
+        messages.forEach((message: Message) => {
+            const newMessage = document.createElement('div');
+            newMessage.classList.add('chat-message-bubble-container');
+            newMessage.innerHTML = `
+            <span class="nickname">${message.user}</span>
             <div class="chat-message-bubble">
                 <span>${message.message}</span> 
             </div>
         `;
-        messagesElement.appendChild(newMessage);
-    });
+            if (message.user === nickname) {
+                newMessage.classList.add('my-messages');
+            }
+            messagesElement?.appendChild(newMessage);
+        });
+    }
+};
+
+const addNickname = () => {
+    nickname = localStorage.getItem('nickname') as string;
+    if (!nickname) {
+        nickname = window.prompt('Rentrer un nom d\'utilisateur') as string;
+        localStorage.setItem('nickname', nickname);
+    }
 };
 
 function render() {
@@ -72,6 +89,7 @@ function render() {
 
 window.onload = () => {
     document.body.innerHTML = render();
+    addNickname();
     getMessages();
     listenMessageForm();
 };
